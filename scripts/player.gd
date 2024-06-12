2edf87dd828e9647ecfe421ae92494faf43ae108
Sahas Poyekar <sahaspoyekar@gmail.com>
2024-06-11 22:21:23 -0700

    added basic npc dialogue

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,17 +1,23 @@
 extends CharacterBody2D
 
-@onready var anim := $AnimationPlayer
+@onready var player_anim := $PlayerAnim
+@onready var weapon_anim := $WeaponAnim
 var walk_speed := 100
 
 func _ready():
-	anim.play("idle")
+	player_anim.play("idle")
+	weapon_anim.play("idle")
 
 func _physics_process(delta):
 	var direction : Vector2 = Input.get_vector("left","right","up","down").normalized()
 	if direction:
 		velocity = direction * walk_speed
-		anim.play("walk")
+		player_anim.play("walk")
 	else:
 		velocity = Vector2.ZERO
-		anim.play("idle")
+		player_anim.play("idle")
+		weapon_anim.play("idle")
+	if Input.is_action_just_pressed("swing"):
+		weapon_anim.play("swing")
+	
 	move_and_slide()
